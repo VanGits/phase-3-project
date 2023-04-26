4985c20c15f85c17bcd9caba28c32f910651e1db
Van Celdran <celdranvan@gmail.com>
2023-04-26 13:21:26 -0700

    added dynamic routes to front end

--- client-side/blog-app/src/components/Main.jsx
+++ client-side/blog-app/src/components/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../styles/Main.css"
+import { useHistory } from 'react-router-dom';
 
 const Main = ({posts}) => {
 
     
-
+    const history = useHistory()
     
 
     
-    const handleClick = () => {
-
+    const handleClick = (id) => {
+        history.push(`/posts/${id}`)
     }
 
     return (
@@ -17,7 +18,7 @@ const Main = ({posts}) => {
             <div className="main-wrapper">
             {posts.map((post) => {
                 return (
-                    <div className='post-wrapper' key={post.id} onClick={handleClick}> 
+                    <div className='post-wrapper' key={post.id} onClick={() => handleClick(post.id)}> 
                          <h2>{post.title}</h2>
                     </div>
                    
